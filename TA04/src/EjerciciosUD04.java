29a29ad9bef76d0f9d08aea518cfdd78a3f4d2cc
Nayara <nay.nivea@gmail.com>
2023-12-01 11:49:32 +0100

    TA04 C2

---
+++ TA04/src/EjerciciosUD04.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+
+/**
+ * @author Nayara Nivea Gomes Santos
+ *
+ */
+public class EjerciciosUD04 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+	/*
+	 *Ejercício 1 
+	 */
+		int num1 = 15;
+		int num2 = 7;
+		
+		System.out.println("La suma de: "+num1+ "+" +num2+ " = "+(num1+num2));
+		System.out.println("La diferencia de: "+num1+ "-" +num2+ " = "+(num1-num2));
+		System.out.println("El producto de: "+num1+ "x" +num2+ " = "+(num1*num2));
+		System.out.println("El cociente de: "+num1+ "/" +num2+ " = "+(num1/num2));
+		System.out.println("El resto de: "+num1+ "%" +num2+ " = "+(num1%num2));
+	/*
+	 *Ejercício 2 
+	 */
+	 int n1 = 5;
+	 double a1 = 2.2;
+	 char c = 't';
+	 
+	 System.out.println("El valor de n: "+n1);
+	 System.out.println("El valor de a: "+a1);
+	 System.out.println("El valor de c: "+c);
+	 
+	 System.out.println("La suma de "+n1+ "+" +a1+ " = "+(n1+a1));
+	 System.out.println("La resta de "+a1+ "-" +n1+ " = "+(a1-n1));
+	 System.out.println("El valor numerico de c es: "+(int)(c));
+	/*
+	 *Ejercício 3 
+	 */
+	 int x = 1;
+	 int y = 3;
+	 double n2 = 62.4;
+	 double m = 2.5;
+	 
+	System.out.println("El valor de x: "+x);
+	System.out.println("El valor de y: "+y);
+	System.out.println("El valor de n: "+n2);
+	System.out.println("El valor de n: "+m);
+	 
+	System.out.println("La suma de "+x+ "+" +y+ " = "+(x+y));
+	System.out.println("La diferencia de "+x+ "-" +y+ " = "+(x-y));
+	System.out.println("El producto de "+x+ "x" +y+ " = "+(x*y));
+	System.out.println("El cociente "+x+ "/" +y+ " = "+(x/y));
+	System.out.println("El resto de "+x+ "%" +y+ " = "+(x%y));
+	
+	System.out.println("La suma de "+n2+ "+" +m+ " = "+(n2+m));
+	System.out.println("La diferencia de "+n2+ "-" +m+ " = "+(n2-m));
+	System.out.println("El producto de "+n2+ "x" +m+ " = "+(n2*m));
+	System.out.println("El cociente "+n2+ "/" +m+ " = "+(n2/m));
+	System.out.println("El resto de "+n2+ "%" +m+ " = "+(n2%m));
+	 
+	/*
+	 *Ejercício 4 
+	 */
+	/*
+	 *Ejercício 5
+	 */
+
+
+}
+
+}
